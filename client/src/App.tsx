d274b254a1691646e589943c1d744d05ae761fef
ashmit-coder <ashmitjagtap2209@gmail.com>
2023-12-28 02:05:14 +0530

    Added brush modifications and prettier

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,27 +1,22 @@
 import Keycloak from "keycloak-js";
-import "./App.css"
+import "./App.css";
 import ToolArray from "./components/ToolArray";
 
-const keycloak = new Keycloak(
-  {
-    url: "http://localhost:8180",
-    realm: "test",
-    clientId: "ashmit",
+const keycloak = new Keycloak({
+  url: "http://localhost:8180",
+  realm: "test",
+  clientId: "ashmit",
+});
 
-  }
-);
-
-keycloak.init({ onLoad: "login-required" })
+keycloak.init({ onLoad: "login-required" });
 
 function App() {
-
   return (
     <div>
       <ToolArray />
       <canvas id="Main-canvas" />
     </div>
-  )
-
+  );
 }
 
-export default App
+export default App;
--- client/src/components/ToolArray.tsx
+++ client/src/components/ToolArray.tsx
@@ -1,105 +1,148 @@
-import { useEffect, useRef } from "react";
-import "../App.css"
+import { useEffect, useRef, useState } from "react";
+import "../App.css";
 import { fabric } from "fabric";
 
+// https://alexsidorenko.com/blog/react-list-rerender/
 
 export default function ToolArray() {
-    let editor = new fabric.Canvas("Main-canvas");
-    let msg = useRef("Drawing Mode");
-    let mode = useRef(false);
+  // let editor = new fabric.Canvas("Main-canvas");
+  const [editor, setEditor] = useState(new fabric.Canvas("Main-canvas"));
+  // let msg = useRef("Drawing Mode");
+  const [msg, setMsg] = useState("Drawing Mode");
+  let mode = useRef(false);
 
-    useEffect(() => {
-        editor = new fabric.Canvas("Main-canvas", {
-            height: 3000,
-            width: 3000,
+  useEffect(() => {
+    // editor = new fabric.Canvas("Main-canvas", {
+    //     height: 3000,
+    //     width: 3000,
 
-        })
+    // })
 
-    }, [])
+    setEditor(
+      new fabric.Canvas("Main-canvas", {
+        height: 3000,
+        width: 3000,
+      }),
+    );
+  }, []);
 
-    function addRect() {
-        var rect = new fabric.Rect({
-            stroke: "black",
-            strokeWidth: 2,
-            width: 100,
-            height: 100,
-            fill: "white",
-            selectable: true
-        });
+  function addRect() {
+    var rect = new fabric.Rect({
+      stroke: "black",
+      strokeWidth: 2,
+      width: 100,
+      height: 100,
+      fill: "white",
+      selectable: true,
+    });
 
-        editor.add(rect);
+    editor.add(rect);
+    setEditor(editor);
+  }
+
+  function addCircle() {
+    var cir = new fabric.Circle({
+      stroke: "black",
+      strokeWidth: 2,
+      radius: 100,
+      fill: "white",
+    });
+
+    editor.add(cir);
+    setEditor(editor);
+  }
+
+  function addTriangle() {
+    var triangle = new fabric.Triangle({
+      stroke: "black",
+      strokeWidth: 2,
+      fill: "white",
+      width: 100,
+      height: 100,
+    });
+
+    editor.add(triangle);
+    setEditor(editor);
+  }
+
+  function addText() {
+    var text = new fabric.Textbox("TEXT");
+    editor.add(text);
+    setEditor(editor);
+  }
+
+  function deleteSelection() {
+    let lst = editor.getActiveObjects();
+
+    lst.forEach((element) => {
+      editor.remove(element);
+    });
+    setEditor(editor);
+  }
+
+  function deleteAll() {
+    editor.getObjects().forEach((element) => {
+      editor.remove(element);
+    });
+    setEditor(editor);
+  }
+
+  function drawingMode() {
+    if (!mode.current) {
+      mode.current = true;
+      setEditor(editor);
+      editor.isDrawingMode = mode.current;
+      // msg.current = "Exit drawing mode";
+      setEditor(editor);
+      setMsg("Exit drawing mode");
+    } else {
+      mode.current = false;
+      setEditor(editor);
+      editor.isDrawingMode = mode.current;
+      // msg.current = "Drawing Mode";
+      setEditor(editor);
+      setMsg("Drawing Mode");
     }
+  }
 
-    function addCircle() {
-        var cir = new fabric.Circle({
-            stroke: "black",
-            strokeWidth: 2,
-            radius: 100,
-            fill: "white",
-        })
-
-        editor.add(cir);
+  function changeBrushColor() {
+    let color = (document.getElementById("Brush-color") as HTMLInputElement)
+      .value;
+    if (color && mode.current) {
+      editor.freeDrawingBrush.color = color;
     }
+  }
 
-    function addTriangle() {
-        var triangle = new fabric.Triangle({
-            stroke: "black",
-            strokeWidth: 2,
-            fill: "white",
-            width: 100,
-            height: 100,
-        })
+  function changeBrushSize() {
+    let size = (document.getElementById("Brush-size") as HTMLInputElement)
+      .value;
 
-        editor.add(triangle);
+    if (size && mode.current) {
+      editor.freeDrawingBrush.width = parseInt(size);
     }
+  }
 
-    function addText() {
-        var text = new fabric.Textbox("TEXT")
-        editor.add(text);
-    }
-
-    function deleteSelection() {
-        let lst = editor.getActiveObjects()
-
-        lst.forEach(element => {
-            editor.remove(element);
-        });
-    }
-
-    function deleteAll() {
-        editor.getObjects().forEach(element => {
-            editor.remove(element);
-
-        })
-    }
-
-    function drawingMode() {
-        if (!mode.current) {
-            mode.current = true;
-            editor.isDrawingMode = mode.current;
-            msg.current = "Exit drawing mode";
-        }
-        else {
-            mode.current = false;
-            editor.isDrawingMode = mode.current;
-            msg.current = "Drawing Mode";
-
-        }
-    }
-
-
-    return (
-        <div className="ToolArray">
-            <button onClick={addRect}>Add Square</button>
-            <button onClick={addCircle}>Add Circle</button>
-            <button onClick={addTriangle}>Add Triangle</button>
-            <button onClick={addText}>Add Textbox</button>
-            <button onClick={deleteSelection}>Delete</button>
-            <button onClick={deleteAll}>Delete All</button>
-            <button onClick={drawingMode}>{msg.current}</button>
-            {/* <button onClick={drawingModeCancel}></button> */}
-
-        </div>
-    )
-
+  return (
+    <div className="ToolArray">
+      <button onClick={addRect}>Add Square</button>
+      <button onClick={addCircle}>Add Circle</button>
+      <button onClick={addTriangle}>Add Triangle</button>
+      <button onClick={addText}>Add Textbox</button>
+      <button onClick={deleteSelection}>Delete</button>
+      <button onClick={deleteAll}>Delete All</button>
+      <button onClick={drawingMode}>{msg}</button>
+      <input
+        type="color"
+        name="Brush-color"
+        id="Brush-color"
+        onChange={changeBrushColor}
+      />
+      <input
+        type="range"
+        name="Brush-size"
+        id="Brush-size"
+        onChange={changeBrushSize}
+      />
+    </div>
+  );
 }
